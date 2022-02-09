3289d86db1e4544de888d37f6ad708c16e84120d
Matt Davison <45557982+matt-davison@users.noreply.github.com>
2022-02-09 13:35:31 -0500

    bootstrapping

---
+++ dist/blockin.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sayHello = void 0;
+function sayHello() {
+    console.log("Hello World");
+}
+exports.sayHello = sayHello;
---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sayHello = void 0;
+var blockin_1 = require("./blockin");
+Object.defineProperty(exports, "sayHello", { enumerable: true, get: function () { return blockin_1.sayHello; } });
