0d104a2c248693b2f7211979c062c626d3b7be83
Kenneth King <kking935@vt.edu>
2022-04-23 18:06:05 -0400

    remove unused method and add client setter, getter

--- src/blockin.ts
+++ src/blockin.ts
@@ -1,27 +1,36 @@
+// Changes: Dont repeate hashing code, instead do it in sha256 and call from sha256AsString
+// Also, immediately return instead of storing as variable before returning?
+// Also, are we even using sha256AsString method??
+
+/**
+ * Encodes a string message as UTF-8 and converts to SHA256 hash as an ArrayBuffer.
+ * Then converts ArrayBuffer to an Array
+ * @param message The string to covert
+ * @returns SHA256 hash of message, as an array
+ */
 export async function sha256(message: string): Promise<Uint8Array> {
-    // encode as UTF-8
-    const msgBuffer = new TextEncoder().encode(message);                    
-
-    // hash the message
-    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
-
-    // convert ArrayBuffer to Array
-    const hashArray = new Uint8Array(hashBuffer);
-
-    return hashArray
+    const msgBuffer = new TextEncoder().encode(message);    // encode as UTF-8               
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);    // hash the message
+    return new Uint8Array(hashBuffer);   // Convert ArrayBuffer to Array
 }
 
-export async function sha256AsString(message: string): Promise<string> {
-        // encode as UTF-8
-        const msgBuffer = new TextEncoder().encode(message);                    
+// /**
+//  * Converts a string message into a SHA256 hash array
+//  * @param message The string to covert
+//  * @returns SHA256 hash of message, as an array
+//  */
+// export async function sha256AsString(message: string): Promise<string> {
+//     const hashArray = Array.from(await sha256(message)); // Call sha256AsArray to get hash array iterable and convert to array
+//     return hashArray.map(b => b.toString(16).padStart(2, '0')).join('');   // convert bytes to hex string 
+// }
 
-        // hash the message
-        const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
-    
-        // convert ArrayBuffer to Array
-        const hashArray = Array.from(new Uint8Array(hashBuffer));
-    
-        // convert bytes to hex string                  
-        const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-        return hashHex;
+
+let client: any = undefined
+
+export function setClient(newClient: any) {
+    client = newClient
+}
+
+export function getClient() {
+    return client
 }
